648c2df76a90d01bd7418119e6fc5e60113e286d
Patrick Ribeiro Duarte <patrickrd@Laptop-de-Patrick.local>
2025-05-26 16:26:42 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return(
-    <div>
-      Home
+    <div className="w-full h-screen flex justify-center items-center bg-gray-300">
+      <Button className="mx-auto my-15 px-14 py-6">
+        Teste
+      </Button>
     </div>
   )
 }
